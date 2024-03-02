d992ebdeae48bb595c19649880ddcd4b05a912c3
Kim-Inyo <101404494+Kim-Inyo@users.noreply.github.com>
2024-03-02 19:47:54 +1000

    Task 1

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script>
+console.log("Hello world!");
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div class="CenterContainer">
+        <div class="HWBox">Hello, world!</div>
+        <div class="HWBox" style="font-family: 'open sans'">Hello, world!</div>
+    </div>
 </template>
+
+<style>
+@import "public/assets/scss/main.scss";
+</style>
